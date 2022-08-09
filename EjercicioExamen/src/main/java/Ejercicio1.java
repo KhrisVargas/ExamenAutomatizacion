dfe7423817415ae64da6b79c81af4394569ac587
KhrisVargas <110554198+KhrisVargas@users.noreply.github.com>
2022-08-09 18:24:38 -0500

    Solo entregara el primer nombre

--- EjercicioExamen/src/main/java/Ejercicio1.java
+++ EjercicioExamen/src/main/java/Ejercicio1.java
@@ -1,7 +1,7 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
         String primerNombre="Cristian";
-        String segundoNombre="Miguel";
+        String segundoNombre="";
 
         System.out.println("Mi primer nombre es "+ primerNombre+
                 " y mi segundo nombre es "+ segundoNombre);
