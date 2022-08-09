e3a6857bb9eea5473cd43467664772bfcfdf7d0b
KhrisVargas <khristian.jp@gmail.com>
2022-08-09 18:12:43 -0500

    subiendo tercer ejercicio

---
+++ EjercicioExamen/src/main/java/Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        int edad=29;
+        String saludo="Mucho Gusto!!!";
+
+        System.out.println("Tengo " + edad +" años, " +saludo);
+
+
+    }
+}
