21f63f3024d8abf96d67816b387517bd34abab22
KhrisVargas <khristian.jp@gmail.com>
2022-08-09 17:46:54 -0500

    subiendo primer ejercicio

---
+++ EjercicioExamen/src/main/java/Ejercicio1.java
@@ -0,0 +1,9 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String primerNombre="Cristian";
+        String segundoNombre="Miguel";
+
+        System.out.println("Mi primer nombre es "+ primerNombre+
+                " y mi segundo nombre es "+ segundoNombre);
+    }
+}
