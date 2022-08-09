4222bd7707716c7ff76f32663bfa0043d373975f
KhrisVargas <110554198+KhrisVargas@users.noreply.github.com>
2022-08-09 18:23:04 -0500

    Cambio desde interfaz web

---
+++ EjercicioExamen/src/main/java/Ejercicio4.java
@@ -0,0 +1,9 @@
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        String primerNombre="";
+        String segundoNombre="";
+
+        System.out.println("Mi primer nombre es "+ primerNombre+
+                " y mi segundo nombre es "+ segundoNombre);
+    }
+}
